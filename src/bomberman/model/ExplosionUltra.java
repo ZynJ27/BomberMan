f96a45da92b369a366e17466ed924fce4812417c
ZynJ27 <165598494+ZynJ27@users.noreply.github.com>
2025-04-08 18:48:08 +0200

    Delete src/bomberman/model/ExplosionUltra.java

--- src/bomberman/model/ExplosionUltra.java
+++
@@ -1,9 +0,0 @@
-package bomberman.model;
-
-public class ExplosionUltra implements ExplosionStrategy {
-
-    @Override
-    public void explotar(int pX, int pY) {
-        GestorTablero.getGestor().getTablero().explotarBomba(pX,pY,20);
-    }
-}
